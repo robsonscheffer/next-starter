72c4a93426e299297c918429caf7f60edef5cd14
Robson Scheffer <robson.scheffer.cardoso@gmail.com>
2021-09-19 21:29:52 -0300

    feat: import and use Example component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
 import { Fragment } from 'react'
+import { Example } from '@/components';
 
 const Home: NextPage = () => {
   return (
-    <Fragment></Fragment>
+    <Fragment>
+      <Example title="Hello" />
+    </Fragment>
   )
 }
 
