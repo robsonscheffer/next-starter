5df9f57c25054d3006b66019b9913687517184ab
Robson Scheffer <robson.scheffer.cardoso@gmail.com>
2021-09-19 21:27:32 -0300

    feat: add root imports config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     '../src/**/*.stories.mdx',
@@ -5,4 +7,12 @@ module.exports = {
     '../stories/**/*.stories.mdx',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+    return config;
+  },
 };
