e86e79e0e141df1d0525427ed6cdac217c388d9a
Robson Scheffer <robson.scheffer.cardoso@gmail.com>
2021-09-20 13:10:32 -0300

    chore: add root paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   stories: [
@@ -8,11 +8,11 @@ module.exports = {
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config, { configType }) => {
-    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules']
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, '../src'),
-    };
-    return config;
+    }
+    return config
   },
-};
+}
